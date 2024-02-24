6a1fea0c1eac454712041b2700fd91b923dabbd8
Inam Ul Haque <inamulhaque952@gmail.com>
2024-02-24 19:04:03 +0530

    done

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+// App.js
+
+import React from 'react';
+import CommentSection from './CommentSection';
+
+function App() {
+  return (
+    <div>
+      <h1>Your App Title</h1>
+      <CommentSection />
+    </div>
+  );
+}
+
+export default App;
